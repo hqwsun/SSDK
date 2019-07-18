16224a01c7d18d689e775a17429b35c4132aced2
huangqiuwei <312944916@qq.com>
2019-07-18 14:22:02 +0800

    更新SDK包

---
+++ SSDK.framework/Headers/SSDK.h
@@ -0,0 +1,25 @@
+//
+//  SSDK.h
+//  SSDK
+//
+//  Created by 头等舱 on 2019/7/17.
+//  Copyright © 2019 huangqiuwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SSDK/SSViewController.h>
+
+/*
+  参考博客： https://www.jianshu.com/p/7d4f0cb11199
+ */
+
+
+//! Project version number for SSDK.
+FOUNDATION_EXPORT double SSDKVersionNumber;
+
+//! Project version string for SSDK.
+FOUNDATION_EXPORT const unsigned char SSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SSDK/PublicHeader.h>
+
+
